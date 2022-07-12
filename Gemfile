adc19f25b7d9f63688f3d0f3ec03ceb27918ca7a
Bernardo C.D.A. Vasconcelos <35749099+bcdavasconcelos@users.noreply.github.com>
2022-07-12 00:31:33 -0300

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in grc.gemspec
+gemspec
+
+gem 'rake', '~> 13.0'
+
+gem 'minitest', '~> 5.0'
+
+gem 'rubocop', '~> 1.21'
+
+gem 'unicode-name', '~> 1.10.0'
---
+++ test/test_grc.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class TestGrc < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Grc::VERSION
+  end
+
+  def test_grc?
+    assert_equal true, 'α'.grc?
+  end
+
+  def test_not_grc?
+    assert_equal false, 'a'.grc?
+  end
+
+  def test_no_downcase_diacritics
+    assert_equal 'α', 'ἄ'.no_downcase_diacritics
+  end
+
+  def test_no_upcase_diacritics
+    assert_equal 'Α', 'Ἀ'.no_upcase_diacritics
+  end
+
+  def test_no_diacritics
+    assert_equal 'αΑ', 'ἄἈ'.no_diacritics
+  end
+
+  def test_tonos_to_oxia
+    assert_equal 'ά', 'ά'.tonos_to_oxia
+  end
+
+  def test_oxia_to_tonos
+    assert_equal 'ά', 'ά'.oxia_to_tonos
+  end
+
+  def test_acute_to_grave
+    assert_equal 'ά', 'ὰ'.grave_to_acute
+  end
+
+  def test_grave_to_acute
+    assert_equal 'ὰ', 'ά'.acute_to_grave
+  end
+
+  def test_tokenization
+    assert_equal %w[σημεῖον δ᾽ ἡ τῶν αἰσθήσεων ἀγάπησις · καὶ γὰρ χωρὶς τῆς χρείας ἀγαπῶνται δι᾽ αὑτάς], 'σημεῖον δ᾽ ἡ τῶν αἰσθήσεων ἀγάπησις· καὶ γὰρ χωρὶς τῆς χρείας ἀγαπῶνται δι᾽ αὑτάς'.tokenize
+  end
+
+  def test_unicode_char
+    assert_equal ['α'], 'α'.unicode_char
+  end
+
+  def test_unicode_name
+    assert_equal ['GREEK SMALL LETTER ALPHA'], 'α'.unicode_name
+  end
+
+  def test_unicode_points
+    assert_equal ['\\u03B1'], 'α'.unicode_points
+  end
+
+  def test_nfc_normalization
+    assert_equal 'ᾄ', 'ᾄ'.nfc
+  end
+
+  def test_nfd_normalization
+    assert_equal 'ᾄ', 'ᾄ'.nfd
+  end
+
+  def test_transliteration_of_greek_to_latin; end
+
+  def test_it_does_something_useful
+    assert true
+  end
+end
